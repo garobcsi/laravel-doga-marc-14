ea0f55fb882621e7c3a4eb6ad80b367c393276fe
garobcsi <gasparrobert0908@gmail.com>
2023-03-14 13:32:14 +0100

    Made create

--- resources/views/character/create.blade.php
+++ resources/views/character/create.blade.php
@@ -5,7 +5,32 @@
 
     <div class="container">
         <div class="row">
+            <h1>Új karakter</h1>
+            <form action="{{route('characters.store')}}" method="POST">
+                @csrf
+                @method('create')
+                <label for="name">Név</label>
+                <input class="form-control" type="text" name="name" id="name">
+                <label for="gender">Nem</label>
+                <input class="form-control" type="text" name="gender" id="gender">
+                <label for="job">Munka</label>
+                <input class="form-control" type="text" name="job" id="job">
+                <label for="wand">Varázspálca</label>
+                <input class="form-control" type="text" name="wand" id="wand">
+                <label for="patronus">Patronus</label>
+                <input class="form-control" type="text" name="patronus" id="patronus">
+                <label for="species">Faj</label>
+                <input class="form-control" type="text" name="species" id="species">
+                <label for="birth">Született</label>
+                <input class="form-control" type="date" name="birth" id="birth">
+                <label for="death">Meghalt</label>
+                <input class="form-control" type="date" name="death" id="death">
+                <label for="house">Ház</label>
+                <select class="form-select" id="house">
 
+                </select>
+                <button class="btn btn-success mt-3" type="submit">Mentés!</button>
+            </form>
         </div>
     </div>
 
